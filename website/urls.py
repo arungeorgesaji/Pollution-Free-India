350e5c769eec3e40d4548c4d6affac08b1154cdb
arungeorgesaji <arungeorgesaji@gmail.com>
2023-01-07 11:44:37 +0530

    pollution

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,11 @@ from django.urls import path
 
 urlpatterns =[
     path('', views.base, name="base"),
-    path('about_us/', views.about_us, name="about_us")
+    path('about_us/', views.about_us, name="about_us"),
+    path('articles/', views.articles, name="articles"),
+    path('pollution_types/', views.pollutions, name="pollution"),
+    path('donate/', views.donate, name="donate"),
+    path('contact_us/', views.contact_us, name="contact"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- website/views.py
+++ website/views.py
@@ -5,3 +5,15 @@ def base(request):
 
 def about_us(request):
     return render(request, "website/about_us.html")
+
+def articles(request):
+    return render(request, "website/articles.html")
+
+def pollutions(request):
+    return render(request, "website/pollutions.html")
+
+def donate(request): 
+    return render(request, "website/donate.html")
+
+def contact_us(request):
+    return render(request, "website/contact_us.html")
