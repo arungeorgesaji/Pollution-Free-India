1f09277060c8381f4042032de93e8e0860b20805
arungeorgesaji <arungeorgesaji@gmail.com>
2023-01-09 23:15:48 +0530

    pollution

--- website/urls.py
+++ website/urls.py
@@ -6,9 +6,19 @@ urlpatterns =[
     path('', views.base, name="base"),
     path('about_us/', views.about_us, name="about_us"),
     path('articles/', views.articles, name="articles"),
-    path('pollution_types/', views.pollutions, name="pollution"),
     path('donate/', views.donate, name="donate"),
     path('contact_us/', views.contact_us, name="contact"),
+    path('air_pollution/', views.air, name="air"),
+    path('water_pollution/', views.water, name="water"),
+    path('noise_pollution/', views.noise, name="noise"),
+    path('littering/', views.littering, name="littering"),
+    path('electromagnetic_pollution/', views.electromagnetic, name="electromagnetic"),
+    path('soil_contamination/', views.soil_contamination, name="soil_contamination"),
+    path('radioactive_contamination/', views.radioactive_contamination, name="radioactive_contamination"),
+    path('light_pollution/', views.light, name="light"),
+    path('thermal_pollution/', views.thermal, name="thermal"),
+    path('visual_pollution/', views.visual, name="visual"),
+    path('plastic_pollution/', views.plastic, name="plastic"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- website/views.py
+++ website/views.py
@@ -9,11 +9,41 @@ def about_us(request):
 def articles(request):
     return render(request, "website/articles.html")
 
-def pollutions(request):
-    return render(request, "website/pollutions.html")
-
 def donate(request): 
     return render(request, "website/donate.html")
 
 def contact_us(request):
     return render(request, "website/contact_us.html")
+
+def air(request): 
+    return render(request, "website/air.html")
+
+def water(request): 
+    return render(request, "website/water.html")
+
+def noise(request): 
+    return render(request, "website/noise.html")
+
+def electromagnetic(request): 
+    return render(request, "website/electromagnetic.html")
+
+def light(request): 
+    return render(request, "website/light.html")
+
+def littering(request): 
+    return render(request, "website/littering.html")
+
+def soil_contamination(request): 
+    return render(request, "website/soil_contamination.html")
+
+def radioactive_contamination(request): 
+    return render(request, "website/radioactive_contamination.html")
+
+def thermal(request): 
+    return render(request, "website/thermal.html")
+
+def visual(request): 
+    return render(request, "website/visual.html")
+
+def plastic(request): 
+    return render(request, "website/plastic.html")
